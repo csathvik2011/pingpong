d342125efb37d47431d3981bf46a85ca83553abd
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-04-30 10:08:19 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "forestgreen";
+const paddle1Color = "lightblue";
+const paddle2Color = "red";
+const paddleBorder = "black";
+const ballColor = "yellow";
+const ballBorderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
